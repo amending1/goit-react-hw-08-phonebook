5269e1a407fea1230ad5463ccc60bc3a17331c4b
amending1 <joasia.sowiak@gmail.com>
2024-03-04 12:33:43 +0100

    redux persist edded

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -2,19 +2,28 @@ import { createSlice } from '@reduxjs/toolkit';
 import { register } from './operations.js';
 
 const initialState = {
-    loading: false,
-}
+  loading: false,
+  user: { name: null, email: null },
+  token: '',
+  isLoggedIn: false,
+};
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    extraReducers: (builder) => {
-        builder.addCase(register.pending, (state) => {state.loading = true})
-        builder.addCase(register.fulfilled, (state, action) => {
-            console.log({action});
-            state.loading = false; })
-        builder.addCase(register.rejected, () => {})
-    },
+  name: 'auth',
+  initialState,
+  extraReducers: builder => {
+    builder.addCase(register.pending, state => {
+      state.loading = true;
+    });
+    builder.addCase(register.fulfilled, (state, action) => {
+      console.log({ action });
+      state.loading = false;
+      state.token = action.payload.token;
+      state.user = action.payload.user;
+      state.isLoggedIn = true;
+    });
+    builder.addCase(register.rejected, () => {});
+  },
 });
 
 export const authReducer = authSlice.reducer;
